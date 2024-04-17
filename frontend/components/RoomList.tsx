91f9804c9142ee956f2118775cb3678ae369188f
Jorge Matos <jisapjisap@gmail.com>
2024-04-17 18:53:02 +0200

    roomlist

--- frontend/components/RoomList.tsx
+++ frontend/components/RoomList.tsx
@@ -8,14 +8,34 @@ import Link from "next/link"
 
 
 const RoomList = ({rooms}: {rooms:any}) => {
+
+  
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+
       {rooms.data.map((room:any) => {
+
+        const imgURL = `http://127.0.0.1:1337${room.attributes.image.data?.attributes.url}`
+
+
         return (
           <div key={room.id}>
             <Link href={`/room/${room.id}`}>
-              room
+              <div className="relative w-full h-[300px] overflow-hidden mb-6">
+                <Image 
+                  src={imgURL}
+                  fill
+                  priority
+                  alt=''
+                />
+              </div>
             </Link>
+
+            <div className="h-[134px] bg-pink-200">
+              <div className="flex">
+
+              </div>
+            </div>
           </div>
         )
       })}
--- frontend/components/Rooms.tsx
+++ frontend/components/Rooms.tsx
@@ -12,7 +12,7 @@ const getRooms = async () => {
 const Rooms = async() => {
 
   const rooms = await getRooms();
- 
+  
 
   return (
     <section>
