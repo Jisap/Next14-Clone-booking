849f17c12a78a49346d44224cde7b4b6e4800b36
Jorge Matos <jisapjisap@gmail.com>
2024-04-19 10:07:32 +0200

    rommlist & tabs

--- frontend/components/RoomList.tsx
+++ frontend/components/RoomList.tsx
@@ -4,7 +4,11 @@ import { useEffect, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import { FaStar, FaStarHalf } from 'react-icons/fa'
-
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+} from '@/components/ui/tabs'
 
 
 
@@ -12,52 +16,78 @@ const RoomList = ({rooms}: {rooms:any}) => {
 
   
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+    <section className="py-16 min-h-[90vh]">
 
-      {rooms.data.map((room:any) => {
+      {/* Image & title */}
+      <div className="flex flex-col items-center">
+        <div className="relative w-[82px] h-[20px]">
+          <Image 
+            src={'/assets/heading-icon.svg'}
+            fill
+            alt=""
+            className="object-contain"
+          />
+        </div>
+        <h2 className="h2 mb-8">Our Rooms</h2>
+      </div>
 
-        const imgURL = `http://127.0.0.1:1337${room.attributes.image.data?.attributes.url}`
+      {/* tabs */}
+      <Tabs defaultValue="all" className="w-[240px] lg:w-[540px] h-[200px] lg:h-auto mb-8 mx-auto">
+        <TabsList className="w-full h-full lg:h-[46px] flex flex-col lg:flex-row">
+          <TabsTrigger className="w-full h-full" value="all">All</TabsTrigger>
+          <TabsTrigger className="w-full h-full" value="single">Single</TabsTrigger>
+          <TabsTrigger className="w-full h-full" value="double">Double</TabsTrigger>
+          <TabsTrigger className="w-full h-full" value="extended">Extended</TabsTrigger>
+        </TabsList>
+      </Tabs>
 
 
-        return (
-          <div key={room.id}>
-            <Link href={`/room/${room.id}`}>
-              <div className="relative w-full h-[300px] overflow-hidden mb-6">
-                <Image 
-                  src={imgURL}
-                  fill
-                  priority
-                  alt=''
-                />
-              </div>
-            </Link>
-
-            <div className="h-[134px]">
-              <div className="flex items-center justify-between mb-6">
-                <div>Capacity - {room.attributes.capacity} person</div>
-                <div className="flex gap-1 text-accent">
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStarHalf />
-                </div>
-              </div>
-
+      {/* room list*/}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+  
+        {rooms.data.map((room:any) => {
+          const imgURL = `http://127.0.0.1:1337${room.attributes.image.data?.attributes.url}`
+          return (
+            <div key={room.id}>
               <Link href={`/room/${room.id}`}>
-                <h3 className="h3">{room.attributes.title}</h3>
+                <div className="relative w-full h-[300px] overflow-hidden mb-6">
+                  <Image 
+                    src={imgURL}
+                    fill
+                    priority
+                    alt=''
+                  />
+                </div>
               </Link>
-
-              <p className="h3 font-secondary font-medium text-accent mb-4">
-                ${room.attributes.price} <span className="text-base text-secondary">/ night</span>
-              </p>
-
+  
+              <div className="h-[134px]">
+                <div className="flex items-center justify-between mb-6">
+                  <div>Capacity - {room.attributes.capacity} person</div>
+                  <div className="flex gap-1 text-accent">
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStarHalf />
+                  </div>
+                </div>
+  
+                <Link href={`/room/${room.id}`}>
+                  <h3 className="h3">{room.attributes.title}</h3>
+                </Link>
+  
+                <p className="h3 font-secondary font-medium text-accent mb-4">
+                  ${room.attributes.price} <span className="text-base text-secondary">/ night</span>
+                </p>
+  
+              </div>
             </div>
-          </div>
-        )
-      })}
-    </div>
+          )
+        })}
+      </div>
+    </section>
   )
+
 }
 
 export default RoomList
