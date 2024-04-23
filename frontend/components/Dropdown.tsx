6776515f504edd31dcc72fe00af3ebd003a15882
Jorge Matos <jisapjisap@gmail.com>
2024-04-23 16:44:31 +0200

    header

---
+++ frontend/components/Dropdown.tsx
@@ -0,0 +1,20 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
+
+const Dropdown = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Dropdown
